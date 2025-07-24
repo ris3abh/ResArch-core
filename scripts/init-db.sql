e49c696df75cd4a6ea4337c11a2b89653949ae50
Rishabh Sharma <rishabh.sharma@spinutech.com>
2025-07-24 14:56:38 -0400

    works

--- scripts/init-db.sql
+++
@@ -1,9 +0,0 @@
--- Initialize Spinscribe database
-CREATE DATABASE IF NOT EXISTS spinscribe;
-CREATE USER IF NOT EXISTS spinscribe_user WITH PASSWORD 'spinscribe_password';
-GRANT ALL PRIVILEGES ON DATABASE spinscribe TO spinscribe_user;
-
--- Enable necessary extensions
-\c spinscribe;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "vector";  -- For vector embeddings if using pgvector
